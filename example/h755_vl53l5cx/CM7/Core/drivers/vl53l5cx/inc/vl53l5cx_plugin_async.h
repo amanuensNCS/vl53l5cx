306d2b87caa171b0003bd5d723bc4bb6a71c19cf
klownfish <github@clownfish.xyz>
2022-01-19 10:42:51 +0100

    add example

---
+++ example/h755_vl53l5cx/CM7/Core/drivers/vl53l5cx/inc/vl53l5cx_plugin_async.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include "vl53l5cx_api.h"
+
+uint8_t vl53l5cx_start_check_data_ready_async(VL53L5CX_Configuration *p_dev);
+uint8_t vl53l5cx_reading_check_data_ready_async(VL53L5CX_Configuration *p_dev);
+uint8_t vl53l5cx_finish_check_data_ready_async(VL53L5CX_Configuration *p_dev,
+		uint8_t *p_result);
+
+uint8_t vl53l5cx_start_get_ranging_data_async(VL53L5CX_Configuration *p_dev);
+uint8_t vl53l5cx_reading_get_ranging_data_async(VL53L5CX_Configuration *p_dev);
+uint8_t vl53l5cx_finish_get_ranging_data_async(VL53L5CX_Configuration *p_dev,
+		VL53L5CX_ResultsData *p_results);
